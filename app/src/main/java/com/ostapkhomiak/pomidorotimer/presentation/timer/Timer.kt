d02fa66227fb85d532f76333abbacddfc65996d1
Ostap Khomiak <hamster.ostap@gmail.com>
2025-09-03 15:04:16 +0200

    refactor : Clean up imports

--- app/src/main/java/com/ostapkhomiak/pomidorotimer/presentation/timer/Timer.kt
+++ app/src/main/java/com/ostapkhomiak/pomidorotimer/presentation/timer/Timer.kt
@@ -1,7 +1,6 @@
 package com.ostapkhomiak.pomidorotimer.presentation.timer
 
 
-import android.widget.NumberPicker
 import androidx.compose.ui.graphics.Color
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -11,16 +10,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -36,7 +31,6 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import com.ostapkhomiak.pomidorotimer.ui.theme.Purple40
 import kotlinx.coroutines.delay
 
