d8ac23be8021cd0de39e6ad4a50b45f7091c8f42
Ostap Khomiak <hamster.ostap@gmail.com>
2025-09-08 15:38:38 +0200

    feat: Add LazyGrid into Inventory

--- app/src/main/java/com/ostapkhomiak/pomidorotimer/presentation/inventory/Inventory.kt
+++ app/src/main/java/com/ostapkhomiak/pomidorotimer/presentation/inventory/Inventory.kt
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,15 +23,31 @@ fun ShowInventory() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(32.dp),
-        contentAlignment = Alignment.Center
+            .padding(32.dp)
     ) {
-        Text("Inventory Test")
-        TomatoCard(Tomato(
+
+        // for debugging purposes
+        val tomato1Test = Tomato(
             id = 1,
             date = LocalDate.now(),
             timeElapsed = "10:00"
-        ))
+        )
+        val tomato2Test = Tomato(
+            id = 2,
+            date = LocalDate.now(),
+            timeElapsed = "25:00"
+        )
+
+        val tomatos = listOf(tomato1Test, tomato2Test, tomato1Test, tomato2Test, tomato1Test, tomato2Test,tomato1Test)
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2)
+        ) {
+            items(tomatos) { tomato ->
+                TomatoCard(tomato)
+            }
+        }
+
 
     }
 
