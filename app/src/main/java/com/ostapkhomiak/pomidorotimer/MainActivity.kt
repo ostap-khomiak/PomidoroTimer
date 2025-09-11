2efd369dbeeaae54d1303c9fe184af26a214e9ff
Ostap Khomiak <hamster.ostap@gmail.com>
2025-09-11 18:00:47 +0200

    refactor: Clean up imports

--- app/src/main/java/com/ostapkhomiak/pomidorotimer/MainActivity.kt
+++ app/src/main/java/com/ostapkhomiak/pomidorotimer/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.ostapkhomiak.pomidorotimer
 
-import android.annotation.SuppressLint
+
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ostapkhomiak.pomidorotimer.presentation.navigation.NavigationInit
 import com.ostapkhomiak.pomidorotimer.ui.theme.PomidoroTimerTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
